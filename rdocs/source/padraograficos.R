8dccad041f5cd3ba71a7645e8a32ae5c0f1a70be
Bruno Gondim Toledo <toledis@gmail.com>
2023-08-24 11:59:37 -0300

    nuvem de palavras
    nuvem de palavras

--- rdocs/source/padraograficos.R
+++ rdocs/source/padraograficos.R
@@ -518,3 +518,94 @@ mtcars_radar <- mtcars |>
 ggradar(mtcars_radar) +
   scale_colour_manual(values = cores_estat) # A função de tema da estat não fica muito legal com esse gráfico; talvez seja melhor não utilizar..
   #theme_estat() 
+
+# ---------------------------------------------------------------------------- #
+
+# 12. Nuvem de palavras ----
+if (!require("pacman")) install.packages("pacman")
+p_load(readr,stringr,tidyverse,lubridate,wordcloud,RColorBrewer,wordcloud2,tm)
+
+# O Input é um df (xlsx,txt,csv...) com apenas uma coluna, em que cada linha é uma string
+# funciona também se cada linha for apenas uma palavra...
+
+df <- read_csv("Conversa do WhatsApp com Gameficação do Time do Hexa 23_2.txt")
+
+df <- as_tibble(df)
+colnames(df) <- 'mensagem'
+
+# Bloco Whatsapp - Este bloco deve ser executado se o input é um txt de log de whatsapp ----
+# Caso contrário, rode apenas o código da linha 14, removendo o `#`, e ignore as linhas 16 a 23.
+# df2 <- df
+
+df$mensagens <- df$mensagem
+df2 <- as_tibble(str_subset(df$mensagens,pattern="entrou usando o link de convite deste grupo",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="Arquivo de mídia oculto",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="https",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="criou o grupo",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="mudou o nome para",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="Mensagem apagada",negate=T))
+df2 <- as_tibble(str_subset(df2$value,pattern="Seu código de segurança com",negate=T))
+
+lista <- list()
+for (i in 1:nrow(df2)){
+  lista[[i]] <- df2[i,1]
+}
+vetor <- unlist(lista)
+
+# Nuvem de palavras:
+
+docs <- Corpus(VectorSource(vetor))
+
+docs <- docs %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace)
+docs <- tm_map(docs, content_transformer(tolower))
+
+dtm <- TermDocumentMatrix(docs) 
+matrix <- as.matrix(dtm) 
+words <- sort(rowSums(matrix),decreasing=TRUE) 
+df <- data.frame(word = names(words),freq=words)
+
+# Estas palavras são filtradas da nuvem. Adicione e exclua conforme necessidade.
+# Para uma lista de stopwords genéricas, utilize a linha 50. caso deseje fazer sua propria lista,
+# utilize o filtro iniciado na linha 53
+
+filtro <- unlist(read_csv("https://gist.githubusercontent.com/alopes/5358189/raw/2107d809cca6b83ce3d8e04dbd9463283025284f/stopwords.txt",
+                          col_names = F))
+
+filtro <- c('que','para','com','isso','tem','por','uma','pra','esse','mais',
+            'dos','das','essa','tá','nas','nem','sem','aos','sobre','aí','pois',
+            'este','esse','dos','ela','pra', 'está', 'mas',"vamos","vai",
+            "estão","nossa","foi","nosso","nos","aqui","ainda","meu","ter",
+            "porque","nossos","são","vão","você","sua","seu","acima","seja",
+            "temos","será","sera","mesmo","pelo","só","so","então","entao",
+            "umas","como","dele","fazendo","galera","alguém","alguem","assim",
+            "número","numero","qualquer","ser","ficar","fazer","ja","já","nossas",
+            "manter","estou","cara","nesse","olha","sair","passar","esses","estas",
+            "dar","pela","esta","apenas","pro","souza","silva","chegar","dessa",
+            "roberto","outra","sendo","estava","gilson","josé","durante","marcus",
+            "marcos","desse","vou","tirar","era","teve","vocês","voces","ontem",
+            "também","tbm","tambem","atraves","através","única","unica","costa",
+            "consegue","as","às","ás","vem","vêm","null","não","ele","la","lá",
+            "sim","boa","estamos","agora","hoje","dia","muito","quem","até",
+            "pode","bom","nome","quando","coisa") # adicionar mais conforme necessidade
+
+#rm(df2,docs,dtm,lista,matrix,filtro,i,vetor,words)
+#gc()
+
+# rode este bloco apenas se desejar filtrar palavras específicas
+df <- df |>
+  filter(!(word %in% filtro)) |> # Aplicando filtros
+  filter(freq >9) # esta linha coloca um mínimo de frequência para as palavras aparecerem na nuvem. Altere conforme necessidade.
+
+# Gera a núvem
+set.seed(150167636) 
+wordcloud(words = df$word, freq = df$freq, min.freq = 2,
+          max.words=200, random.order=FALSE, rot.per=0.35, # Ajuste o parâmetro max.words conforme necessidade. O máximo é 200.
+          colors=cores_estat)
+
+#write.table(filtro,"filtradas.txt",row.names = F,col.names = F)
+
+# ---------------------------------------------------------------------------- #
+
